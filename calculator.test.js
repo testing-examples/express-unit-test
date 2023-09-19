e9652a14078b152c83d207af3dcab761ba960b2e
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-09-19 19:59:01 +0300

    this should fail

--- calculator.test.js
+++ calculator.test.js
@@ -5,5 +5,5 @@ test('calling the endpoint with one number should result in the number itself', 
   });
 
 test('calling the endpoint with two nummber should result in the sum of the numbers', () => {
-    expect(calculator.calculateSum(4,5)).toBe(91);
+    expect(calculator.calculateSum(4,5)).toBe(8);
   });
